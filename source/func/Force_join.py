5714b6c321f2915513c3cc6a611e377083296036
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-07-09 11:19:44 +0300

    Uploaded Successfully

---
+++ source/func/Force_join.py
@@ -0,0 +1,14 @@
+# FORCE JOIN BY ANUBIS #
+async def FORCE_JOIN(app):
+    try :
+        await app.join_chat("CU_FG")
+    except :
+        pass
+    try :
+        await app.join_chat("wvvwv3")
+    except :
+        pass
+    try :
+        await app.join_chat("CBHll")
+    except :
+        pass
